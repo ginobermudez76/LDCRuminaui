f77d5820b5ecacd830130e90bc8e159db0885e79
GINOMAXILMILIANO BERMUDEZ SANTOS <ginomaxilmiliano.bermudez@ister.edu.ec>
2024-02-24 20:59:41 -0500

    validacion de tipos de documento y creacion de carpetas de forma automatica para solicitudes de cada usuario, verificacion si documento se encuentra existente y renombrar el nuevo

--- admin/tbsolicitud.php
+++ admin/tbsolicitud.php
@@ -1,12 +1,11 @@
 <?php
-include '../includes/config.php'; //incluyendo la conexión de la base de datos
-include '../includes/header.php'; //incluyendo la cabecera común
+include '../includes/config.php'; // incluyendo la conexión de la base de datos
+include '../includes/header.php'; // incluyendo la cabecera común
 if (!isset($_SESSION['usuario_admin'])) {
     header("Location: /Ayudantias-1/admin/login.php");
     exit();
 }
 $usuario_id = $_SESSION['usuario_id'];
-
 try {
     $stmt = $conn->prepare("CALL mostrar_solicitudes(:usuario_id)");
     $stmt->bindParam(':usuario_id', $usuario_id, PDO::PARAM_INT);
@@ -15,7 +14,7 @@ try {
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
-//obtener lista de tipo de solicitud
+// obtener lista de tipo de solicitud
 try {
     $stmt = $conn->prepare("SELECT id_tipo, name_tipo FROM solicitud_tipo");
     $stmt->execute();
@@ -28,43 +27,69 @@ try {
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $descripcion = $_POST['descripcion'];
-    $valor = $_POST['valor'];
-    $tipo = $_POST['tipo_id'];
-    if (isset($_FILES['documento']) && $_FILES['documento']['error'] == 0) {
-        $directorioDestino = "../uploads/documentos/solicitudes/";
-        $archivo = $directorioDestino . basename($_FILES['documento']['name']);
-        $tipoArchivo = strtolower(pathinfo($archivo, PATHINFO_EXTENSION));
+    $valor = isset($_POST['valor']) ? $_POST['valor'] : null;
 
-        // Lista de extensiones permitidas
+    // Verificar si el valor es numérico
+    if (!is_numeric($valor)) {
+        $valor = null;
+    }
+
+    $tipo = $_POST['tipo_id'];
+
+    // Obtener el nombre de usuario
+    try {
+        $stmt = $conn->prepare("SELECT nombre_usuario FROM usuarios WHERE id = :usuario_id");
+        $stmt->bindParam(':usuario_id', $usuario_id, PDO::PARAM_INT);
+        $stmt->execute();
+        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+        $nombre_usuario = $usuario['nombre_usuario'];
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+
+    // Directorio de destino para el documento
+    $directorioDestino = "../uploads/documentos/solicitudes/";
+
+    if (isset($_FILES['documento']) && $_FILES['documento']['error'] == 0) {
+        $nombreArchivo = basename($_FILES['documento']['name']);
+        $archivo = $directorioDestino . $nombre_usuario . '/' . $nombreArchivo;
+
+        // Verificar si el archivo ya existe y renombrarlo si es necesario
+        $contador = 1;
+        while (file_exists($archivo)) {
+            $nombreArchivo = pathinfo($_FILES['documento']['name'], PATHINFO_FILENAME) . '_' . $contador . '.' . pathinfo($_FILES['documento']['name'], PATHINFO_EXTENSION);
+            $archivo = $directorioDestino . $nombre_usuario . '/' . $nombreArchivo;
+            $contador++;
+        }
+
+        // Verificar la extensión del archivo
+        $tipoArchivo = strtolower(pathinfo($nombreArchivo, PATHINFO_EXTENSION));
         $extensionesPermitidas = array('pdf', 'doc', 'docx', 'txt');
 
-        // Verificar si la extensión del archivo está en la lista de extensiones permitidas
-        if (in_array($tipoArchivo, $extensionesPermitidas)) {
-            if (move_uploaded_file($_FILES["documento"]["tmp_name"], $archivo)) {
-                // El archivo se cargó correctamente
-            } else {
+        if (!in_array($tipoArchivo, $extensionesPermitidas)) {
+            $error = "El archivo no es un documento válido";
+        } else {
+            // Mover el archivo al directorio de destino
+            if (!move_uploaded_file($_FILES["documento"]["tmp_name"], $archivo)) {
                 $error = "Hubo un error al cargar el documento";
             }
-        } else {
-            $error = "El archivo no es un documento válido";
         }
     } else {
         // Manejo en el caso de que no se haya seleccionado ningún archivo
         $archivo = "";
     }
 
-
-
     try {
         $conn->beginTransaction(); // Inicia una transacción
 
+        // Insertar la solicitud en la base de datos
         $stmt = $conn->prepare("INSERT INTO solicitud (s_fecha, s_doc, s_valor, tipo, solicitante, descripcion) VALUES (NOW(), ?, ?, ?, ?, ?)");
         $stmt->execute([$archivo, $valor, $tipo, $usuario_id, $descripcion]);
 
-        // Obtén el ID de la solicitud insertada
+        // Obtener el ID de la solicitud insertada
         $solicitudId = $conn->lastInsertId();
 
-        // Llama al procedimiento almacenado
+        // Llamar al procedimiento almacenado para actualizar el departamento encargado
         $stmt = $conn->prepare("CALL actualizar_departamento_encargado_proc(?, ?)");
         $stmt->execute([$tipo, $solicitudId]);
 
@@ -79,6 +104,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+
+
 
 <div class="container mt-4">
     <h2 class="gestionar">Solicitudes</h2>
@@ -108,18 +135,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <?php endif; ?>
                     </td>
                     <td>
-                        Tipo:<?php echo htmlspecialchars($solicitud['tipo']); ?><br>
-                        Valor solicitado: $ <?php echo htmlspecialchars($solicitud['s_valor']); ?><br>
-                        Descripción: <?php echo htmlspecialchars($solicitud['descripcion']); ?>
-                
+                        Tipo: <?php echo htmlspecialchars($solicitud['tipo']); ?><br>
+                        <?php if (!empty($solicitud['s_valor'])) : ?>
+                            Monto: $<?php echo htmlspecialchars($solicitud['s_valor']); ?><br>
+                        <?php endif; ?>
+                        <?php if (!empty($solicitud['descripcion'])) : ?>
+                            Descripción: <?php echo htmlspecialchars($solicitud['descripcion']); ?>
+                        <?php endif; ?>  
                     </td>
+
                     <td>
                         Departamento: <?php echo htmlspecialchars($solicitud['departamento_encargado']); ?><br>
                         Persona: <?php echo htmlspecialchars($solicitud['encargado']); ?>
                     </td>
                     <td><?php echo htmlspecialchars($solicitud['estado']); ?></td>
                     <td>
-                    <button type="button" class="btn btn-secondary btn-sm">Editar</button>
+                        <?php
+
+                        if ($solicitud['estado'] == 'En tramite') {
+                        ?>
+                            <button type="button" class="btn btn-secondary btn-sm">Editar</button>
+                        <?php
+                        }
+                        ?>
+
                         <button class="btn btn-danger btn-sm" onclick="confirmarEliminacion(<?php echo $solicitud['s_id']; ?>)">Eliminar</button>
                     </td>
                 </tr>
@@ -164,52 +203,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </div>
 </div>
-<!-- Modal para editar solicitud -->
-<div class="modal fade" id="editarSolicitudModal" tabindex="-1" aria-labelledby="editarSolicitudModalLabel" aria-hidden="true" onsubmit="return validarTipo()">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="agregarSolicitudModalLabel">Edición</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form action="tbsolicitud.php" method="POST" enctype="multipart/form-data">
-                    <div class="mb-3">
-                        <label for="documento" class="form-label">Documento</label>
-                        <input type="file" class="form-control" id="documento" name="documento">
-                    </div>
-                    <div class="mb-3">
-                        <label for="tipo" class="form-label">Tipo</label>
-                        <select class="form-select" id="tipo_id" name="tipo_id">
-                            <option value="">Tipo de solicitud</option>
-                            <?php foreach ($tipos as $tipo) : ?>
-                                <option value="<?php echo $tipo['id_tipo']; ?>"><?php echo htmlspecialchars($tipo['name_tipo']); ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label for="descripcion" class="form-label">Descripción</label>
-                        <textarea class="form-control" id="descripcion" name="descripcion" rows="3" value="<?php echo htmlspecialchars($solicitud['descripcion']); ?>"></textarea>
-                    </div>
-                    <div class="mb-3">
-                        <label for="valor" class="form-label">Valor solicitado</label>
-                        <input type="number" class="form-control" id="valor" name="valor" step="0.01">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Agregar Solicitud</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+
 <script>
-    function validarTipo(){
+    function validarTipo() {
         var seleccionTipo = document.getElementById("tipo_id").value;
         if (seleccionTipo === "") {
             alert("Por favor, seleccione un tipo de solicitud");
             return false;
         }
+
+        // Validar el tipo de archivo seleccionado
+        var archivoInput = document.getElementById("documento");
+
+
+        var archivo = archivoInput.files[0];
+        var extensionesPermitidas = ['pdf', 'doc', 'docx', 'txt'];
+        var extension = archivo.name.split('.').pop().toLowerCase();
+
+        if (!extensionesPermitidas.includes(extension)) {
+            alert("El archivo seleccionado no es válido. Por favor, seleccione un archivo PDF, DOC, DOCX o TXT");
+            return false;
+        }
         return true;
     }
+
     function confirmarEliminacion(idSolicitud) {
         var confirmacion = confirm("¿Está seguro que desea eliminar esta solicitud?");
 
@@ -245,5 +262,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </script>
 
 <?php include '../includes/footer.php'; ?>
-
-quiero mandar el id de usuario y el id de la solicitud al modal editarSolicitudModal al precionar el boton editar, para que me obtenga los datos de la solicitud y los ponga en los campos del modal para editar, y luego quiero mandar los datos actualizados al modal
