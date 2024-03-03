22dd744121106c66527df92ea4606db3a6574e8e
GINOMAXILMILIANO BERMUDEZ SANTOS <ginomaxilmiliano.bermudez@ister.edu.ec>
2024-03-03 12:31:17 -0500

    agregados estilos de la seccion de deportes

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@ try {
 
 try {
     // Obtener datos de la tabla "deporte"
-    $stmtDeportes = $conn->prepare("SELECT nombre, descripcion, imagen FROM deportes");
+    $stmtDeportes = $conn->prepare("SELECT * FROM deportes");
     $stmtDeportes->execute();
     $deportes = $stmtDeportes->fetchAll(PDO::FETCH_ASSOC);
 
@@ -76,16 +76,33 @@ try {
         echo '<div class="column">';
         // Iterar sobre las imágenes en el grupo
         foreach ($grupo as $depor) {
+            echo '<div class="column">';
             echo '<div class="card-img-top">';
             echo '<img src="../uploads/deportes/' . htmlspecialchars(basename($depor['imagen'])) . '" class="card-img" alt="' . htmlspecialchars($depor['nombre']) . '">';
             echo '<div class="overlay">';
-            echo '<div class="deporte-nombre">' . htmlspecialchars($depor['nombre']) . '</div>';
+            echo '<div class="deporte-nombre" data-deporte-id="' . htmlspecialchars($depor['id']) . '">' . htmlspecialchars($depor['nombre']) . '</div>';
+            echo '</div>';
             echo '</div>';
             echo '</div>';
         }
         echo '</div>';
     }
     ?>
+</div>
+
+<div class="modal" id="myModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <div class="modal-body">
+                <div id="carousel" class="carousel"></div>
+                <!-- Corregir la clase del contenedor de la superposición -->
+                <div class="overlay2">
+                    <div class="col-md-6" id="descripcionDeporte"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 
@@ -161,7 +178,44 @@ try {
     </div>
 </div>
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+$(document).ready(function(){
+    // Cuando se hace clic en el nombre de deporte
+    $(".deporte-nombre").click(function(){
+        var deporte_id = $(this).data("deporte-id");
+        // Hacer una petición AJAX para obtener las imágenes del carrusel
+        $.ajax({
+            url: "obtener_imagenes.php",
+            method: "GET",
+            data: { deporte_id: deporte_id },
+            success: function(response){
+                // Agregar las imágenes al carrusel
+                $("#carousel").html(response);
+                // Mostrar el modal
+                $("#myModal").css("display", "block");
+            }
+        });
 
+        // Hacer una petición AJAX para obtener la descripción del deporte
+        $.ajax({
+            url: "obtener_descripcion_deporte.php",
+            method: "GET",
+            data: { deporte_id: deporte_id },
+            success: function(response){
+                // Agregar la descripción del deporte
+                $("#descripcionDeporte").html(response);
+            }
+        });
+    });
+
+    // Cuando se hace clic en el botón de cerrar del modal
+    $(".close").click(function(){
+        $("#myModal").css("display", "none");
+    });
+});
+
+</script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-bYQVJwS/Jt2f7fSFb4hKQVaPvhIrm+PoH6n/TYYJ8WlxFgyC3m8M2MUpM3Il7eJb" crossorigin="anonymous"></script>
 
