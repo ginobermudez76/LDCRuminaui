5c73f29a0d94b71c4c39a29443562af629c47bf4
Gino Maximiliano Bermúdez Santos <bginomaximiliano@gmail.com>
2026-07-11 22:45:16 -0500

    correccion de rol

--- backend/app/Http/Controllers/UsuarioController.php
+++ backend/app/Http/Controllers/UsuarioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Usuario;
+use App\Models\Rol;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
